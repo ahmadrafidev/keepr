be92f4e3d2f7bedf0ddcade578dbbc7c2e1c9644
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2025-05-19 13:52:45 +0700

    fix state in sidebar

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -7,20 +7,20 @@ import { DEFAULT_SIDEBAR_CONFIG } from '@/components/types/sidebar'
 import { cn } from '@/lib/utils'
 
 function HomeLayout() {
-  const { isCollapsed } = useSidebar()
+  const { state } = useSidebar()
 
   return (
     <div className="flex min-h-screen flex-col">
       <div className="flex flex-1">
         <div className={cn(
           "shrink-0 transition-all duration-300",
-          isCollapsed ? "w-16" : "w-60"
+          state === "collapsed" ? "w-16" : "w-60"
         )}>
           <CustomSidebar sidebarConfig={DEFAULT_SIDEBAR_CONFIG} />
         </div>
         <main className={cn(
           "flex-1 p-8 transition-all duration-300",
-          isCollapsed ? "ml-0" : "ml-0"
+          state === "collapsed" ? "ml-0" : "ml-0"
         )}>
           <HomeContent />
         </main>
